eb731768d217c411dd01716f5d665207588f40ac
npullen2000 <80427203+npullen2000@users.noreply.github.com>
2023-01-18 14:36:38 -0600

    Update index.js

--- front/index.js
+++ front/index.js
@@ -1,57 +1,76 @@
-//selecting all required elements
-const dropArea = document.querySelector(".drag-area"),
-dragText = dropArea.querySelector("header"),
-button = dropArea.querySelector("button"),
-input = dropArea.querySelector("input");
-let file; //this is a global variable and we'll use it inside multiple functions
+//DOM
+const $ = document.querySelector.bind(document);
 
-button.onclick = ()=>{
-  input.click(); //if user click on the button then the input also clicked
-}
+//APP
+let App = {};
+App.init = (function() {
+	//Init
+	function handleFileSelect(evt) {
+		const files = evt.target.files; // FileList object
 
-input.addEventListener("change", function(){
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = this.files[0];
-  dropArea.classList.add("active");
-  showFile(); //calling function
-});
+		//files template
+		let template = `${Object.keys(files)
+			.map(file => `<div class="file file--${file}">
+     <div class="name"><span>${files[file].name}</span></div>
+     <div class="progress active"></div>
+     <div class="done">
+	<a href="" target="_blank">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 1000 1000">
+		<g><path id="path" d="M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z"</g>
+		</svg>
+						</a>
+     </div>
+    </div>`)
+			.join("")}`;
 
+		$("#drop").classList.add("hidden");
+		$("footer").classList.add("hasFiles");
+		$(".importar").classList.add("active");
+		setTimeout(() => {
+			$(".list-files").innerHTML = template;
+		}, 1000);
 
-//If user Drag File Over DropArea
-dropArea.addEventListener("dragover", (event)=>{
-  event.preventDefault(); //preventing from default behaviour
-  dropArea.classList.add("active");
-  dragText.textContent = "Release to Upload File";
-});
+		Object.keys(files).forEach(file => {
+			let load = 2000 + (file * 2000); // fake load
+			setTimeout(() => {
+				$(`.file--${file}`).querySelector(".progress").classList.remove("active");
+				$(`.file--${file}`).querySelector(".done").classList.add("anim");
+			}, load);
+		});
+	}
 
-//If user leave dragged File from DropArea
-dropArea.addEventListener("dragleave", ()=>{
-  dropArea.classList.remove("active");
-  dragText.textContent = "Drag & Drop to Upload File";
-});
+	// trigger input
+	$("#triggerFile").addEventListener("click", evt => {
+		evt.preventDefault();
+		$("input[type=file]").click();
+	});
 
-//If user drop File on DropArea
-dropArea.addEventListener("drop", (event)=>{
-  event.preventDefault(); //preventing from default behaviour
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = event.dataTransfer.files[0];
-  showFile(); //calling function
-});
+	// drop events
+	$("#drop").ondragleave = evt => {
+		$("#drop").classList.remove("active");
+		evt.preventDefault();
+	};
+	$("#drop").ondragover = $("#drop").ondragenter = evt => {
+		$("#drop").classList.add("active");
+		evt.preventDefault();
+	};
+	$("#drop").ondrop = evt => {
+		$("input[type=file]").files = evt.dataTransfer.files;
+		$("footer").classList.add("hasFiles");
+		$("#drop").classList.remove("active");
+		evt.preventDefault();
+	};
 
-function showFile(){
-  let fileType = file.type; //getting selected file type
-  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-  if(validExtensions.includes(fileType)){ //if user selected file is an image file
-    let fileReader = new FileReader(); //creating new FileReader object
-    fileReader.onload = ()=>{
-      let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
-      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-    }
-    fileReader.readAsDataURL(file);
-  }else{
-    alert("This is not an Image File!");
-    dropArea.classList.remove("active");
-    dragText.textContent = "Drag & Drop to Upload File";
-  }
-}
+	//upload more
+	$(".importar").addEventListener("click", () => {
+		$(".list-files").innerHTML = "";
+		$("footer").classList.remove("hasFiles");
+		$(".importar").classList.remove("active");
+		setTimeout(() => {
+			$("#drop").classList.remove("hidden");
+		}, 500);
+	});
+
+	// input change
+	$("input[type=file]").addEventListener("change", handleFileSelect);
+})();
